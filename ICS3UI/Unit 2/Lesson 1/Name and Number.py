0a8c2f5d8d20b4b2652a6ac3ea44a480069aeaa6
Kevin Gao <37745638+KevinBoxuGao@users.noreply.github.com>
2019-02-19 10:56:35 -0500

    Add files via upload

---
+++ ICS3UI/Unit 2/Lesson 1/Name and Number.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+number1 = input("Enter your first 16 numbers on the front of your credit card: ")
+number2 = int(input("Enter your second wacky number: "))
+
+print("Congratulations")
+print()
