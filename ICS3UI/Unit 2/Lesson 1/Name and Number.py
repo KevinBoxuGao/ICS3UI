87d31d47322d1c4dbc80ac8a55a60c66b5bff90e
Kevin Gao <37745638+KevinBoxuGao@users.noreply.github.com>
2019-02-19 10:57:25 -0500

    Delete Name and Number.py

--- ICS3UI/Unit 2/Lesson 1/Name and Number.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name? ")
-number1 = input("Enter your first 16 numbers on the front of your credit card: ")
-number2 = int(input("Enter your second wacky number: "))
-
-print("Congratulations")
-print()
