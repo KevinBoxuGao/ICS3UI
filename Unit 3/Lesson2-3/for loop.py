da5ce1c95731c64b6e25fdc9ea9e8e680d555cc0
Kevin Gao <37745638+KevinBoxuGao@users.noreply.github.com>
2019-02-27 10:48:37 -0500

    Add files via upload

---
+++ Unit 3/Lesson2-3/for loop.py
@@ -0,0 +1,11 @@
+from time import *
+
+target = input("Who would you like to hack?")
+
+print("hacking into their mainframe")
+print("initializing..")
+
+for i in range(101):
+    print("initializing..")
+    sleep(1)
+    print(str(i)+"%")
