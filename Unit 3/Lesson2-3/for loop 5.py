5a627524d07e23d0a47460167707a0dc83aff39a
Kevin Gao <37745638+KevinBoxuGao@users.noreply.github.com>
2019-03-08 10:49:32 -0500

    Delete for loop 5.py

--- Unit 3/Lesson2-3/for loop 5.py
+++
@@ -1,6 +0,0 @@
-marks = int(input("How many marks do you want to enter? "))
-total = 0
-for i in range(marks):
-    mark = int(input("enter one of your marks: "))
-    total = total + mark
-print(total/marks)
