c155254b8faaacbd4f00176397bf06ff6b677487
Kevin Gao <kevingao2003@gmail.com>
2019-03-26 21:17:18 -0400

    Basketball shot done

--- Unit 3/Assignment 3/Basketball shot.py
+++ Unit 3/Assignment 3/Basketball shot.py
@@ -9,6 +9,8 @@ screen.pack()
 #background
 screen.create_rectangle(0,700,1000,1000, fill="grey", outline="grey")
 
+screen.create_oval(700,100, 700+50,100+50, fill="yellow")
+
 #ball
 ballWidth = 40
 
@@ -38,12 +40,45 @@ screen.create_rectangle(netx+ballWidth+5, yStart-80, netx+ballWidth+20+5, yStart
 screen.create_rectangle(netx+ballWidth+5, yStart+10, netx+ballWidth+20+5, 800-40, fill="red")
 
 #person jump
-#for i in range():
+xStartP = 100
+yStartP = 700
+
+legheight = 100
+legwidth = 40
+
+bodyheight = 80
+bodywidth = 40
+
+headwidth = 40
+
+
+
+
+
+
+
+for i in range(1,126):
+    yStartP = (gravityStrength/2) * i**2 - 4 * i + yStartP
+    person = [[xStartP, yStartP],[xStartP+legwidth,yStartP-legheight], [xStartP,yStartP-legheight],[xStartP+bodywidth, yStartP-legheight-bodyheight], [xStartP,yStartP-legheight-bodyheight], [xStartP+headwidth, yStartP-legheight-bodyheight-headwidth], [xStartP+(bodywidth/2),yStartP-legheight-bodyheight], [xStartP+(bodywidth/2),yStartP-legheight-bodyheight+20], [xStartP+(bodywidth/2)+bodyheight,yStartP-legheight-bodyheight-10], [xStartP+(bodywidth/2)+bodyheight,yStartP-legheight-bodyheight+20-10]]
+
+    leg = screen.create_rectangle(person[0], person[1], fill="blue")
+    body = screen.create_rectangle(person[2], person[3], fill="red")
+    head = screen.create_oval(person[4], person[5],fill="tan")
+    arm = screen.create_polygon(person[6],person[7],person[9],person[8], fill="red")    
+    ball1 = screen.create_oval(person[8][0], person[8][1]+ballWidth, person[8][0]+ballWidth, person[8][1], fill="orange") 
+
+    screen.update()
+    sleep(0.02)
+    screen.delete(leg, body,head,arm, ball1)
+
+    if person[8][1] + ballWidth <= yStart:
+        break   
+
 
 #initial ball arc
-for i in range(1,126):
+grounded = False
 
-    
+for i in range(1,126):
     x1 =  xSpeed*i + xStart
     y1 = (gravityStrength/2) * i**2 - ySpeed * i + yStart
     
@@ -52,9 +87,29 @@ for i in range(1,126):
 
     ball = screen.create_oval(  x1,  y1,  x2,  y2,  fill="orange",  outline="black")
 
+    if grounded == False:
+        yStartP = (gravityStrength/2) * i**2 - 0.5 * i + yStartP
+        person = [[xStartP, yStartP],[xStartP+legwidth,yStartP-legheight], [xStartP,yStartP-legheight],[xStartP+bodywidth, yStartP-legheight-bodyheight], [xStartP,yStartP-legheight-bodyheight], [xStartP+headwidth, yStartP-legheight-bodyheight-headwidth], [xStartP+(bodywidth/2),yStartP-legheight-bodyheight], [xStartP+(bodywidth/2),yStartP-legheight-bodyheight+20], [xStartP+(bodywidth/2)+bodyheight,yStartP-legheight-bodyheight-10], [xStartP+(bodywidth/2)+bodyheight,yStartP-legheight-bodyheight+20-10]]
+
+        leg = screen.create_rectangle(person[0], person[1], fill="blue")
+        body = screen.create_rectangle(person[2], person[3], fill="red")
+        head = screen.create_oval(person[4], person[5],fill="tan")
+        arm = screen.create_polygon(person[6],person[7],person[9],person[8], fill="red")    
+
+        
+        if yStartP >= 700:
+            grounded = True
+
     screen.update()
     sleep(0.02)
+        
     screen.delete(ball)
+    
+    if yStartP <= 700:
+        screen.delete(leg, body, arm, head)
+     
+
+   
 
     if y1 > yStart:
         break
@@ -67,6 +122,10 @@ for i in range(1,120):
     y2 = y1 + ballWidth
     
     ball = screen.create_oval(  x1,  y1,  x2,  y2,  fill="orange",  outline="black")
+    
     screen.update()
     sleep(0.02)
     screen.delete(ball)
+
+
+screen.mainloop()
