be938eb2327b86ed26b2afb7166ca3673d9ac897
Kevin Gao <kevingao2003@gmail.com>
2019-04-11 21:50:04 -0400

    animation done

--- Unit 4/Assignment/Sunset timelapse.py
+++ Unit 4/Assignment/Sunset timelapse.py
@@ -10,6 +10,8 @@ screen.pack()
 def convertRGB(r, g , b):
     return '#%02x%02x%02x' % (r, g, b)
 
+#starting Values
+#ground and sky
 groundRGB = [124, 252, 0]
 skyRGB = [0, 175, 255]
 
@@ -41,37 +43,11 @@ for i in range(numClouds):
     clouds.append(cloudBlobs)
     cloudSize.append(blobSize)
 
-
-for f in range(1000):
-    for i in range(numClouds):
-        if cloudXStart[i] - 100 - 50 >= 800:
-            cloudXStart[i] = -150
-            for j in range(50):
-                cloudX[i][j] = cloudX[i][j] - 800 - 200 - 50 -50             
-
-        cloudRGB[0] = cloudRGB[0] - 1
-        cloudRGB[1] = cloudRGB[1] - 1
-        cloudRGB[2] = cloudRGB[2] - 1
-        cloudXStart[i] = cloudXStart[i] + 5
-        for j in range(50):
-            cloudX[i][j] = cloudX[i][j] + 5
-            clouds[i][j] = screen.create_oval(cloudX[i][j] + cloudSize[i][j], cloudY[i][j] + cloudSize[i][j], cloudX[i][j] - cloudSize[i][j], cloudY[i][j] - cloudSize[i][j], fill="white", outline="white")
-
-
-
-    screen.update()
-    sleep(0.03)
-
-    for i in range(numClouds):
-        for j in range(50):
-            screen.delete(clouds[i][j])
-    
-
-
 #stars
 numStars = 500
 constY = []
 sizes = []
+starRGB = [0, 0, 0]
 
 for i in range(numStars):
     diceRoll = randint(1, 100)
@@ -85,6 +61,8 @@ for i in range(numStars):
         sizes.append(1)
     constY.append(randint(0-20, 600+20))
 
+
+#animation driver
 while True:
     #sun
     radius = 40
@@ -93,18 +71,17 @@ while True:
     sunSpeed = 4 
 
     #stars
-    
     x = []
     y = []
     stars = []
-
     for i in range(numStars):
-        x.append(randint(0-800, 800-800))
+        x.append(randint(0, 800))
         y.append(constY[i])
         stars.append(0)
 
     #day to night
     for f in range(255):
+        #change ground and sky
         groundColor = convertRGB(groundRGB[0],groundRGB[1],groundRGB[2])
         skyColor = convertRGB(skyRGB[0],skyRGB[1],skyRGB[2])
 
@@ -121,11 +98,31 @@ while True:
         sky = screen.create_rectangle(0, 0, 810, 600, fill=skyColor, outline=skyColor)
         ground = screen.create_rectangle(0, 600, 810, 810, fill=groundColor, outline=groundColor)
 
+        #draw clouds
+        cloudRGB[0] = cloudRGB[0] - 1
+        cloudRGB[1] = cloudRGB[1] - 1
+        cloudRGB[2] = cloudRGB[2] - 1
+        for i in range(numClouds):
+            if cloudXStart[i] - 100 - 50 >= 800:
+                cloudXStart[i] = -150
+                for j in range(50):
+                    cloudX[i][j] = cloudX[i][j] - 800 - 200 - 50 -50             
+            
+            cloudXStart[i] = cloudXStart[i] + 5
+            for j in range(50):
+                cloudX[i][j] = cloudX[i][j] + 5
+                clouds[i][j] = screen.create_oval(cloudX[i][j] + cloudSize[i][j], cloudY[i][j] + cloudSize[i][j], cloudX[i][j] - cloudSize[i][j], cloudY[i][j] - cloudSize[i][j], fill=convertRGB(cloudRGB[0], cloudRGB[1], cloudRGB[2]), outline=convertRGB(cloudRGB[0], cloudRGB[1], cloudRGB[2]))
+
         screen.update()
         sleep(0.03)
         screen.delete(sky, ground)
 
-    for f in range(500):
+        for i in range(numClouds):
+            for j in range(50):
+                screen.delete(clouds[i][j])
+    
+    #draw stars during night
+    for f in range(350):
         #overlap ground
         groundColor = convertRGB(groundRGB[0],groundRGB[1],groundRGB[2])
         if groundRGB[0] > 0:
@@ -133,17 +130,28 @@ while True:
         if groundRGB[1] > 30:
             groundRGB[1] = groundRGB[1] - 1
 
-        #draw stars when it is night
-        for i in range(numStars):
+        #draw stars 
+        #fade stars in
+        if f < 51 and starRGB[0] < 255:
+            starRGB[0] = starRGB[0] +5
+            starRGB[1] = starRGB[1] +5
+            starRGB[2] = starRGB[2] +5
 
-            if x[i] - sizes[i] >= 800 and f < 300:
+        #fade stars out
+        if f >= 350 - 51 and starRGB[0] > 0:
+            starRGB[0] = starRGB[0] -5
+            starRGB[1] = starRGB[1] -5
+            starRGB[2] = starRGB[2] -5
+
+        for i in range(numStars):
+            if x[i] - sizes[i] >= 800:
                 x[i] = 0
             else:
                 x[i] = x[i] + 4
             if x[i] >= 0:
                 y[i] = (1/2500)*(x[i]-400)**2+constY[i] - 20 
         
-            stars[i] = screen.create_oval(x[i] - sizes[i], y[i] - sizes[i], x[i] + sizes[i], y[i] + sizes[i], fill="white", outline="white")   
+            stars[i] = screen.create_oval(x[i] - sizes[i], y[i] - sizes[i], x[i] + sizes[i], y[i] + sizes[i], fill=convertRGB(starRGB[0], starRGB[1], starRGB[2]), outline=convertRGB(starRGB[0], starRGB[1], starRGB[2]))  
 
         ground = screen.create_rectangle(0, 600, 810, 810, fill=groundColor, outline=groundColor)
 
@@ -153,9 +161,14 @@ while True:
         for i in range(numStars):
             screen.delete(stars[i])
 
-            
+    #reset star color
+    starRGB[0] = 255
+    starRGB[1] = 255
+    starRGB[2] = 255
+ 
     #night to day
     for f in range(255):
+        #set sky and ground color
         groundColor = convertRGB(groundRGB[0],groundRGB[1],groundRGB[2])
         skyColor = convertRGB(skyRGB[0],skyRGB[1],skyRGB[2])
 
@@ -171,17 +184,62 @@ while True:
 
         sky = screen.create_rectangle(0,0, 800, 600, fill=skyColor, outline=skyColor)
 
+        #draw setting with sun
         if f > 255 - ((600+2*radius)/sunSpeed):
             y1 = y1 - sunSpeed
             sun = screen.create_oval(x1-radius, y1-radius, x1+radius, y1+radius, fill="yellow", outline="yellow")
             ground = screen.create_rectangle(0, 600, 800, 800, fill=groundColor, outline=groundColor)
+            
+            #draw clouds
+            cloudRGB[0] = cloudRGB[0] + 1
+            cloudRGB[1] = cloudRGB[1] + 1
+            cloudRGB[2] = cloudRGB[2] + 1
+            for i in range(numClouds):
+                if cloudXStart[i] - 100 - 50 >= 800:
+                    cloudXStart[i] = -150
+                    for j in range(50):
+                        cloudX[i][j] = cloudX[i][j] - 800 - 200 - 50 -50             
+
+                cloudXStart[i] = cloudXStart[i] + 5
+                for j in range(50):
+                    cloudX[i][j] = cloudX[i][j] + 5
+                    clouds[i][j] = screen.create_oval(cloudX[i][j] + cloudSize[i][j], cloudY[i][j] + cloudSize[i][j], cloudX[i][j] - cloudSize[i][j], cloudY[i][j] - cloudSize[i][j], fill=convertRGB(cloudRGB[0], cloudRGB[1], cloudRGB[2]), outline=convertRGB(cloudRGB[0], cloudRGB[1], cloudRGB[2]))
+            
             screen.update()
             sleep(0.03)
             screen.delete(ground, sky, sun)
+
+
+            for i in range(numClouds):
+                for j in range(50):
+                    screen.delete(clouds[i][j])
+
+        #draw setting without sun
         else:
             ground = screen.create_rectangle(0, 600, 800, 800, fill=groundColor, outline=groundColor)
+            
+            #draw clouds
+            cloudRGB[0] = cloudRGB[0] + 1
+            cloudRGB[1] = cloudRGB[1] + 1
+            cloudRGB[2] = cloudRGB[2] + 1
+            for i in range(numClouds):
+                if cloudXStart[i] - 100 - 50 >= 800:
+                    cloudXStart[i] = -150
+                    for j in range(50):
+                        cloudX[i][j] = cloudX[i][j] - 800 - 200 - 50 -50             
+
+                cloudXStart[i] = cloudXStart[i] + 5
+                for j in range(50):
+                    cloudX[i][j] = cloudX[i][j] + 5
+                    clouds[i][j] = screen.create_oval(cloudX[i][j] + cloudSize[i][j], cloudY[i][j] + cloudSize[i][j], cloudX[i][j] - cloudSize[i][j], cloudY[i][j] - cloudSize[i][j], fill=convertRGB(cloudRGB[0], cloudRGB[1], cloudRGB[2]), outline=convertRGB(cloudRGB[0], cloudRGB[1], cloudRGB[2]))
+            
             screen.update()
             sleep(0.03)
             screen.delete(ground, sky)
+
+            for i in range(numClouds):
+                for j in range(50):
+                    screen.delete(clouds[i][j])
+
 
         
