01b1bf1a2250a27bc89b4cf7b303c89456177b30
Kevin <me@bradtravery.com>
2019-06-10 21:45:56 -0400

    basic game functioning

--- Game.py
+++ Game.py
@@ -6,53 +6,50 @@ from random import *
 root = Tk()
 screen = Canvas(root, width=600, height=900, background="white")
 
-#data model for pieces of trash
+#object model for a piece of trash
 class Trash:
-	def __init__(self, x, y, width, height, kind, trashGIF): #set object variables
+	def __init__(self, x, y, width, height, type, trashGIF): #set object variables on initialization
 		self.x = x
 		self.y = y
 		self.width = width
 		self.height = height
 
-		self.kind = kind
+		self.type = type
 		self.trashGIF = trashGIF
 
 		self.clicked = False
 
-	def remake(self): #used to reinitialize the garbage if it is missed
-		xDrawings.append(xDrawing(self.x, self.y))
-		#reassigns all of the values of the trash object
-		self.kind = choice(garbageKind) #chooses random garbage kind
-		kindIndex = garbageKind.index(self.kind)
-		self.trashGIF = choice(allTrashItemsGIFS[kindIndex])
-		item = trashItemGIF(self.kind) #creates temporary object to store values of gif
+	def reset(self): #Method, reset trash object variables, giving the illusion that a new trash item is created
+		self.type = choice(trashType) #chooses random trash type
+		typeIndex = trashType.index(self.type) 
+		self.trashGIF = choice(allTrashItemsGIFS[typeIndex])
+		item = trashItemGIF(self.type) #creates temporary object to store values of gif
 		self.trashGIF = item.gif 
 		self.width = item.dimensions[0]
 		self.height = item.dimensions[1]
 		self.x = randint(0+(self.width//2), 600-(self.width//2))
 		self.y = randint(-900+(self.height//2), 0-(self.height//2))
-	def update(self): #set object location
+
+	def lostLife(self): #Method, 
+		xDrawings.append(xDrawing(self.x, self.y)) #add xDrawing in place of trash that wasn't placed in the correct bin in time
+	
+	def update(self): #Method, set object location
 		global lives, score
 		self.y = self.y + ySpeed  # increases position of trash
-		# checks if piece of garbage has exceeded limit and is not clicked on
+		# checks if piece of trash has exceeded limit and is not clicked on
 		if self.y+(self.height//2) > 900 and self.clicked == False:
 			lives = lives - 1
-			self.remake()
-		elif self.y+(self.height//2) > 700 and self.clicked == True: #checks if the trash is in the right bin
-			kindIndex = garbageKind.index(self.kind)
-			if trashBinArea[kindIndex][0] >= self.x-(self.width//2) or trashBinArea[kindIndex][1] >= self.x+(self.width//2):
-				score = score + 1
-			else: 
-				lives = lives - 1
-				self.remake()
+			self.lostLife()
+			self.reset()
+		
 #object containing trash item gif information
 class trashItemGIF:
-	def __init__(self, kind):
-		self.kind = kind
-		kindIndex = garbageKind.index(kind)
-		self.gif = choice(allTrashItemsGIFS[kindIndex])
-		trashGIFIndex = allTrashItemsGIFS[kindIndex].index(self.gif)
-		self.dimensions = trashItemDimensions[kindIndex][trashGIFIndex]
+	def __init__(self, type):
+		self.type = type
+		typeIndex = trashType.index(type)
+		self.gif = choice(allTrashItemsGIFS[typeIndex])
+		trashGIFIndex = allTrashItemsGIFS[typeIndex].index(self.gif)
+		self.dimensions = trashItemDimensions[typeIndex][trashGIFIndex]
 
 #object for X drawing
 class xDrawing:
@@ -70,11 +67,11 @@ def getDistance(x1, y1, x2, y2):
 # SET UP OUR VALUES
 # set our global variables
 def setInitialValues():
-	global score, lives, piecesOfTrash, ySpeed, garbageKind
+	global score, lives, piecesOfTrash, ySpeed, trashType
 	global xMouse, yMouse, mouseDown, escPressed, clickedItem
 	global glassGIF, organicGIF, paperGIF, plasticGIF, allTrashItemsGIFS
 	global trashItemDimensions, trashBinArea
-	global garbageBinsGIF
+	global trashBinsGIF
 	
 
 	global xDrawings #array of all drawings of X's that indicate a lost life
@@ -88,13 +85,13 @@ def setInitialValues():
 	plasticGIF = [PhotoImage(file="imgs/plastic/plastic1.gif"), PhotoImage(file="imgs/plastic/plastic2.gif"), PhotoImage(file="imgs/plastic/plastic3.gif"), PhotoImage(file="imgs/plastic/plastic4.gif")]
 	#collection of all trash item images
 	allTrashItemsGIFS = [paperGIF, organicGIF, glassGIF, plasticGIF]
-	#multi-dimensional array that has heirarchy of [kind of item][dimensions for each item]
+	#multi-dimensional array that has heirarchy of [type of item][dimensions for each item]
 	trashItemDimensions = [[[80,60],[111,71],[106,76]], [[57,74],[47,46],[46,58],[66,62]], [[29,82],[23,76],[23,81],[22,44]], [[28,80],[52,64],[60,79],[39,39]]]
 	#array that stores regions of each bin in the x axis
-	trashBinArea = [[0,250],[250,500],[500,750],[750,900]]
+	trashBinArea = [[0,150],[150,300],[300,450],[450,600]]
 
-	garbageBinsGIF = PhotoImage(file = "imgs/garbage bins.gif")
-	garbageKind = ['glass', 'organic', 'paper', 'plastic']
+	trashBinsGIF = PhotoImage(file = "imgs/trash bins.gif")
+	trashType = ['paper', 'organic', 'glass', 'plastic']
 
 	score = 0
 	lives = 3
@@ -105,16 +102,16 @@ def setInitialValues():
 
 #background drawings
 def drawTrashBins():
-	screen.create_image(300, 800, anchor="center", image=garbageBinsGIF)
+	screen.create_image(300, 800, anchor="center", image=trashBinsGIF)
 
 #Functions for interacting with trash items
-# initializes values for pieces of garbage at the start of the game
+# initializes values for pieces of trash at the start of the game
 def createTrash():
 	global trash
 	trash = []
 	for i in range(piecesOfTrash):
-		kind = choice(garbageKind) #chooses random garbage kind
-		item = trashItemGIF(kind)
+		type = choice(trashType) #chooses random trash type
+		item = trashItemGIF(type)
 
 		trashGIF = item.gif 
 		width = item.dimensions[0]
@@ -123,7 +120,7 @@ def createTrash():
 		x = randint(0+(width//2), 600-(width//2))
 		y = randint(-900+(height//2), 0-(height//2))
 
-		trash.append(Trash(x, y, width, height, kind, trashGIF))
+		trash.append(Trash(x, y, width, height, type, trashGIF))
 
 # draws each piece of trash to the screen
 def drawTrash():
@@ -143,13 +140,23 @@ def updateTrash():
 
 # GETS CALLED CONSTANTLY FOR ANIMATION
 # draw all of our graphics
+def updateStatistics():
+	global scoreDisplay, livesDisplay
+
+	scoreMessage = "Score: " + str(score)
+	livesMessage = "Lives: " + str(lives)
+
+	scoreDisplay = screen.create_text(100, 36, text = scoreMessage, font = "Times 36", anchor=CENTER, fill="black")
+	livesDisplay = screen.create_text(500, 36, text = livesMessage, font = "Times 36", anchor=CENTER, fill="black")
 def drawObjects():
 	drawTrash()
-
+	updateStatistics()
 # delete our drawings
 def deleteObjects():
 	deleteTrash()
 	#check if timers of x drawings is up
+	screen.delete(scoreDisplay)
+	screen.delete(livesDisplay)
 	for i in xDrawings:
 		if i.time == 0:
 			screen.delete(i.drawing[0])
@@ -197,12 +204,24 @@ def mouseMotionHandler(event):
 		
 # gets called when mouse is released
 def mouseReleaseHandler(event):
-	global xMouse, yMouse, mouseDown, clickedItem
+	global xMouse, yMouse, mouseDown, clickedItem, lives, score, trash
 
 	mouseDown=False
 
 	xMouse=event.x
 	yMouse=event.y
+
+	#checks if trash is placed in the right bin
+	if trash[clickedItem].y+(trash[clickedItem].height//2) > 700:
+		typeIndex = trashType.index(trash[clickedItem].type)
+		if trashBinArea[typeIndex][0] <= trash[clickedItem].x and trash[clickedItem].x <= trashBinArea[typeIndex][1]:
+			score = score + 1
+			trash[clickedItem].reset()
+		else: 
+			lives = lives - 1
+
+			trash[clickedItem].lostLife()
+			trash[clickedItem].reset()
 
 	trash[clickedItem].clicked = False
 	clickedItem = 'None'
@@ -215,7 +234,7 @@ def keyDownHandler(event):
 
 
 
-def endGame():
+def stopGame():
 	root.destroy()
 
 def runGame():
@@ -230,7 +249,7 @@ def runGame():
 
 		deleteObjects()
 		updateObjects()
-	endGame()
+	stopGame()
 	print(score)
 
 # At the bottom
